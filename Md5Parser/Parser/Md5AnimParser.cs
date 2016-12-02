7bcaa63fbec244a1dbe21e7bd4d4132c7acd9e27
Bartłomiej Kołodziejczyk <bartek.kol93@gmail.com>
2016-12-02 22:53:21 +0100

    ParseList method, parsers moved to seperate files

---
+++ Md5Parser/Parser/Md5AnimParser.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using Parser.Data.Anim;
+
+namespace Parser
+{
+    public class Md5AnimParser : Md5Parser
+    {
+        public Md5AnimFile Parse(IEnumerable<Token> tokens)
+        {
+            TokenQueue = new Queue<Token>(tokens);
+
+            ParseKeyword("MD5Version");
+            var version = ParseInt("version");
+
+            ParseKeyword("commandline");
+            var commandLineParameters = ParseQuotedString("command line parameters");
+
+
+            ParseKeyword("numFrames");
+            var numFrames = ParseInt("number of frames");
+
+            ParseKeyword("numJoints");
+            var numJoints = ParseInt("number of joints");
+
+            ParseKeyword("frameRate");
+            var frameRate = ParseInt("frame rate");
+
+            ParseKeyword("numAnimatedComponents");
+            var numAnimatedComponents = ParseInt("number of animated components");
+
+
+            var skeletonJoints = ParseSkeletonJoints();
+            AssertCorrectListSize("skeleton joints", skeletonJoints, numJoints);
+
+            var bounds = ParseBounds();
+            AssertCorrectListSize("frame bounds", bounds, numFrames);
+
+            var baseFrames = ParseBaseFrames();
+            AssertCorrectListSize("base frames", baseFrames, numJoints);
+
+            var frames = ParseFrames(numAnimatedComponents);
+            AssertCorrectListSize("frames", frames, numFrames);
+
+            return new Md5AnimFile(version, commandLineParameters, frameRate, skeletonJoints, bounds, baseFrames, frames);
+        }
+
+        private IList<SkeletonJoint> ParseSkeletonJoints()
+        {
+            return ParseList("hierarchy", () =>
+            {
+                var boneName = ParseQuotedString("bone name");
+                var parentIndex = ParseInt("parent index");
+                var flags = ParseInt("flags");
+                var startIndex = ParseInt("start index");
+
+                return new SkeletonJoint(boneName, parentIndex, flags, startIndex);
+            });
+        }
+
+        private IList<Bound> ParseBounds()
+        {
+            return ParseList("bounds", () =>
+            {
+                ParseLeftParenthesis();
+                var minX = ParseFloat("min x");
+                var minY = ParseFloat("min y");
+                var minZ = ParseFloat("min z");
+                ParseRightParenthesis();
+
+                ParseLeftParenthesis();
+                var maxX = ParseFloat("max x");
+                var maxY = ParseFloat("max y");
+                var maxZ = ParseFloat("max z");
+                ParseRightParenthesis();
+
+                return new Bound(minX, minY, minZ, maxX, maxY, maxZ);
+            });
+        }
+
+        private IList<JointPosition> ParseBaseFrames()
+        {
+            return ParseList("baseframe", () =>
+            {
+                ParseLeftParenthesis();
+                var positionX = ParseFloat("position x");
+                var positionY = ParseFloat("position y");
+                var positionZ = ParseFloat("position z");
+                ParseRightParenthesis();
+
+                ParseLeftParenthesis();
+                var orientX = ParseFloat("orientation x");
+                var orientY = ParseFloat("orientation y");
+                var orientZ = ParseFloat("orientation z");
+                ParseRightParenthesis();
+
+                return new JointPosition(positionX, positionY, positionZ, orientX, orientY, orientZ);
+            });
+        }
+
+        private IList<Frame> ParseFrames(int numAnimatedComponents)
+        {
+            var frames = new List<Frame>();
+            while (TokenQueue.Peek().Type == TokenType.Word && TokenQueue.Peek().Value == "frame")
+            {
+                ParseKeyword("frame");
+
+                var frameIndex = ParseInt("frame index");
+
+                ParseLeftBrace();
+
+                var parameters = new List<double>();
+                while (TokenQueue.Peek().Type == TokenType.Float || TokenQueue.Peek().Type == TokenType.Integer)
+                {
+                    parameters.Add(ParseFloat("frame parameter"));
+                }
+
+                ParseRightBrace();
+
+                AssertCorrectListSize("frame parameters", parameters, numAnimatedComponents);
+
+                frames.Add(new Frame(frameIndex, parameters));
+            }
+
+            return frames;
+        }
+    }
+}
---
+++ Md5Parser/Parser/Md5Parser.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Parser
+{
+    public abstract class Md5Parser
+    {
+        protected Queue<Token> TokenQueue;
+
+        private void ParseSimpleToken(TokenType expectedTokenType, string errorMessage)
+        {
+            if (TokenQueue.Dequeue().Type != expectedTokenType)
+            {
+                throw new Exception(errorMessage);
+            }
+        }
+
+        protected void ParseLeftParenthesis()
+        {
+            ParseSimpleToken(TokenType.LParen, "left parenthesis '(' expected");
+        }
+
+        protected void ParseRightParenthesis()
+        {
+            ParseSimpleToken(TokenType.RParen, "right parenthesis ')' expected");
+        }
+
+        protected void ParseLeftBrace()
+        {
+            ParseSimpleToken(TokenType.LBrace, "left brace '{' expected");
+        }
+
+        protected void ParseRightBrace()
+        {
+            ParseSimpleToken(TokenType.RBrace, "right brace '}' expected");
+        }
+
+        protected int ParseInt(string name)
+        {
+            var token = TokenQueue.Dequeue();
+            if (token.Type != TokenType.Integer && token.Type != TokenType.Float)
+            {
+                throw new Exception(name + " expected (integer number)");
+            }
+            return int.Parse(token.Value);
+        }
+
+        protected double ParseFloat(string name)
+        {
+            var token = TokenQueue.Dequeue();
+            if (token.Type != TokenType.Float && token.Type != TokenType.Integer)
+            {
+                throw new Exception(name + " expected (floating point number)");
+            }
+            return double.Parse(token.Value, CultureInfo.InvariantCulture);
+        }
+
+        protected string ParseQuotedString(string name)
+        {
+            var token = TokenQueue.Dequeue();
+            if (token.Type != TokenType.QuotedString)
+            {
+                throw new Exception(name + " expected (quoted string)");
+            }
+            return token.Value;
+        }
+
+        protected void ParseKeyword(string name)
+        {
+            var token = TokenQueue.Dequeue();
+            if (!(token.Type == TokenType.Word && token.Value == name))
+            {
+                throw new Exception(name + " keyword expected");
+            }
+        }
+
+        protected void AssertCorrectListSize<T>(string name, IList<T> list, int expectedSize)
+        {
+            if (list.Count != expectedSize)
+            {
+                throw new Exception(string.Format(
+                    "number of parsed {0} ({1}) is different than expected ({2})", name, list.Count, expectedSize));
+            }
+        }
+
+        protected IList<T> ParseList<T>(string blockName, Func<T> parseItem)
+        {
+            ParseKeyword(blockName);
+
+            ParseLeftBrace();
+
+            var items = new List<T>();
+            while (TokenQueue.Peek().Type != TokenType.RBrace)
+            {
+                items.Add(parseItem());
+            }
+            ParseRightBrace();
+
+            return items;
+        }
+    }
+}
